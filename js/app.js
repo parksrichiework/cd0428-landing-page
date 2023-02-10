dbb912511c215d24ffafdc58868eb07ac8e63c7b
Richie Parks <parksrichiework@gmail.com>
2023-02-10 17:30:50 -0600

    Building Navigation Bar
    - assigned variable for navigation
    - assigned variable for unordered list element
    - created function for adding new <li> to <ul> container
    - assigned global array for all new <li> elements

--- js/app.js
+++ js/app.js
@@ -22,7 +22,14 @@
  * Define Global Variables
  * 
 */
+// navigation element 
+const nav = document.querySelector('.navbar__menu');
 
+// unordered list inside navigation element 
+const ulNav = document.querySelector('#navbar__list')
+
+// all unordered list items (saved as an array)
+let listItems = document.querySelectorAll('#list-item')
 
 /**
  * End Global Variables
@@ -30,7 +37,14 @@
  * 
 */
 
-
+// declare function to create a new list item 
+function addLi(title){
+    let newLi = document.createElement('li')
+    newLi.setAttribute('style', 'font-size: 1em; color: darkblue; padding: 12px 1em;')
+    newLi.setAttribute('id', 'list-item')
+    newLi.textContent = title
+    ulNav.appendChild(newLi)   
+}
 
 /**
  * End Helper Functions
@@ -39,6 +53,18 @@
 */
 
 // build the nav
+
+
+// create new list items inside <ul>
+addLi('one')
+addLi('two')
+addLi('three')
+
+
+const firstItem = listItems[0]
+
+// assign style properties to nav 
+nav.setAttribute('style', 'background-color: #fff; height: 3em;')
 
 
 // Add class 'active' to section when near top of viewport
