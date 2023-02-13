830124a9c19492b09d55bdfa4f167131c86b0fbd
Richie Parks <parksrichiework@gmail.com>
2023-02-13 17:18:34 -0600

    Updated Javascript
    - added function to create new <li> in <nav>
    - added function to run operator when you click <li> in <nav>
    - added function to respond to user scrolling
    - added function to calculate scroll y-position and console.log the value

--- js/app.js
+++ js/app.js
@@ -29,7 +29,13 @@ const nav = document.querySelector('.navbar__menu');
 const ulNav = document.querySelector('#navbar__list')
 
 // all unordered list items (saved as an array)
-let listItems = document.querySelectorAll('#list-item')
+
+
+// declare array for all sections 
+let sections = document.querySelectorAll('section')
+
+//
+
 
 /**
  * End Global Variables
@@ -41,7 +47,7 @@ let listItems = document.querySelectorAll('#list-item')
 function addLi(title){
     let newLi = document.createElement('li')
     newLi.setAttribute('style', 'font-size: 1em; color: darkblue; padding: 12px 1em;')
-    newLi.setAttribute('id', 'list-item')
+    newLi.setAttribute('class', 'list-item')
     newLi.textContent = title
     ulNav.appendChild(newLi)   
 }
@@ -53,15 +59,11 @@ function addLi(title){
 */
 
 // build the nav
-
-
 // create new list items inside <ul>
-addLi('one')
-addLi('two')
-addLi('three')
+addLi(sections[0].id)
+addLi(sections[1].id)
+addLi(sections[2].id)
 
-
-const firstItem = listItems[0]
 
 // assign style properties to nav 
 nav.setAttribute('style', 'background-color: #fff; height: 3em;')
@@ -69,9 +71,40 @@ nav.setAttribute('style', 'background-color: #fff; height: 3em;')
 
 // Add class 'active' to section when near top of viewport
 
+window.addEventListener('scroll', sectionActive)
+
+  function sectionActive(){
+    // function to calculate srcoll height 
+    let scroll = window.scrollY    
+    if(scroll === sections[0].scrollTop){
+      console.log('Section 1 active')
+    }
+    if(scroll == 200){
+      console.log('hello')
+    }
+    if(scroll == 200){
+      console.log('hello')
+    }           
+      console.log(scroll)     
+    }  
 
 // Scroll to anchor ID using scrollTO event
+ 
 
+  window.addEventListener('click', (event)=>{
+    const navItems = document.querySelectorAll('.list-item')
+    
+      if(event.target === navItems[0]){
+        console.log('section 1')
+      }
+      if(event.target === navItems[1]){
+        console.log('section 2')
+      }
+      if(event.target === navItems[2]){
+        console.log('section 3')
+      }      
+
+  })
 
 /**
  * End Main Functions
