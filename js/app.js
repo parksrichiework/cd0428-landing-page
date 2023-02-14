e9acc98f0e2c53c54211cb8dfac10e8cda311b44
Richie Parks <parksrichiework@gmail.com>
2023-02-14 16:28:47 -0600

    Update app.js
    updated event listener for adding "active" class to a section when in view

--- js/app.js
+++ js/app.js
@@ -29,7 +29,7 @@ const nav = document.querySelector('.navbar__menu');
 const ulNav = document.querySelector('#navbar__list')
 
 // all unordered list items (saved as an array)
-
+let navItems = document.querySelectorAll('.list-item')
 
 // declare array for all sections 
 let sections = document.querySelectorAll('section')
@@ -43,7 +43,7 @@ let sections = document.querySelectorAll('section')
  * 
 */
 
-// declare function to create a new list item 
+// RUBRIC NEED: declare function to create a new list item 
 function addLi(title){
     let newLi = document.createElement('li')
     newLi.setAttribute('style', 'font-size: 1em; color: darkblue; margin: 1em; padding: 0 1em;')
@@ -58,7 +58,7 @@ function addLi(title){
  * 
 */
 
-// build the nav
+// RUBRIC NEED: build the nav
 // create new list items inside <ul>
 addLi(sections[0].id)
 addLi(sections[1].id)
@@ -69,51 +69,71 @@ addLi(sections[2].id)
 nav.setAttribute('style', 'background-color: #fff; height: 3em;')
 
 
-// Add class 'active' to section when near top of viewport
+// RUBRIC NEED: Add class 'active' to section when near top of viewport  
 
-window.addEventListener('scroll', sectionActive)
-
-  function sectionActive(){
-    // variable to hold all <li> in <nav> 
-    const navItems = document.querySelectorAll('.list-item')
+  //listen for scroll
+  window.addEventListener('scroll', ()=>{
 
     // function to calculate srcoll height 
     let scroll = window.scrollY
-    let header = document.querySelector('.main__hero')
+    let height = window.innerHeight
 
-    if(scroll > header.scrollHeight && scroll < sections[0].scrollHeight){
-      sections[0].classList.toggle('your-active-class')
-      console.log('Section 1 active')
-    }
-    if(scroll > sections[0].scrollHeight && scroll < sections[1].scrollHeight){
-      sections[1].classList.toggle('your-active-class')
-      console.log('Section 2 active')
-    }
-    if(scroll > sections[1].scrollHeight && scroll < sections[2].scrollHeight){
-      sections[2].classList.toggle('your-active-class')
-      console.log('Section 3 active')
-    }
-   
-     
-      console.log(scroll)     
-    }  
+    //console.log the window scroll height every time you scroll
+    console.log(scroll , height)
 
-// Scroll to anchor ID using scrollTO event
- 
+    //calculate the position of each section
+    for (let i=0; i <= navItems.length ; i++){  
+      let top1 = sections[0].getBoundingClientRect()      
+      let top2 = sections[1].getBoundingClientRect()      
+      let top3 = sections[2].getBoundingClientRect()  
+
+      let listItems = document.querySelectorAll('.list-item')
+      
+      //change color of <li> based on active section
+      // add or remove "active" class based on active section
+      if (top1.top <= window.innerHeight && top1.bottom >= window.innerHeight){
+        listItems[0].style.background = 'lightgrey'
+        listItems[1].style.background = 'white'
+        listItems[2].style.background = 'white'
+        sections[0].classList.add('active')
+        sections[1].classList.remove('active')
+        sections[2].classList.remove('active')
+      } else if (top2.top <= window.innerHeight && top2.bottom >= window.innerHeight){
+        listItems[1].style.background = 'lightgrey'
+        listItems[0].style.background = 'white'
+        listItems[2].style.background = 'white'
+        sections[1].classList.add('active')
+        sections[0].classList.remove('active')
+        sections[2].classList.remove('active')
+      } else if (top3.top <= window.innerHeight && top3.bottom >= window.innerHeight){
+        listItems[2].style.background = 'lightgrey'
+        listItems[0].style.background = 'white'
+        listItems[1].style.background = 'white'
+        sections[2].classList.add('active')
+        sections[0].classList.remove('active')
+        sections[1].classList.remove('active')
+      }    
+    }
+  })  
+
+
+// RUBRIC NEED: Scroll to anchor ID using scrollTO event 
 
   window.addEventListener('click', (event)=>{
     const navItems = document.querySelectorAll('.list-item')
     
+    //loop through each <li> in <nav> to listen for a click
       for (let i=0; i <= navItems.length ; i++){
+
+        //if certain <li> is clicked, scroll to the corresponding section
         if(event.target === navItems[i]){
           sections[i].scrollIntoView({
             behavior: 'smooth'
-          })
-          navItems[i].style.backgroundColor= 'lightgrey'
-          console.log('Scrolled to section 1')
-        } else {
-          navItems[i].style.backgroundColor= '#fff'
-        }
+          })        
+                      
+        //console log the id of the section that was scrolled to
+        console.log(`Scrolled to ${sections[i].id}`)
+        } 
       }                 
     })
 
